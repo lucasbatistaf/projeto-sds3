df7e08d66bd72129c91a0e933f5432d593a4c3fe
Lucas Batista <lucaasbatista182@hotmail.com>
2021-09-12 17:24:17 -0300

    Home, navigation

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -5,32 +5,32 @@ import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
 const Dashboard = () => {
-    return (
+  return (
     <>
-        <NavBar />
-        <div className="container">
-          <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-  
-          <div className="row px-3">
-            <div className="col-sm-6">
-              <h5 className="text-center text-secondary">Taxa de sucesso %</h5>
-              <BarCharts />
-            </div>
-            <div className="col-sm-6">
-              <h5 className="text-center text-secondary">Todas as Vendas</h5>
-              <DonutChart />            
-            </div>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso %</h5>
+            <BarCharts />
           </div>
-  
-          <div className="py-3">
-          <h2 className="text-primary">Todas as Vendas</h2>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas as Vendas</h5>
+            <DonutChart />
           </div>
-  
-          <DataTable />
         </div>
-        <Footer />
-      </>
-    );
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas as Vendas</h2>
+        </div>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Dashboard;
